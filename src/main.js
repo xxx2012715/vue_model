d71b29e7ea1feb37be7663578dd696a9eb55b335
kite <943443868@qq.com>
2020-04-02 09:36:03 +0800

    vue4.0初始化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './assets/res/border.css'
+import './assets/res/reset.css'
+
+import FastClick from 'fastclick'
+FastClick.attach(document.body)
+
 Vue.config.productionTip = false
 
 new Vue({

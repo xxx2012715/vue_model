fc812df17a164f194c3bc958d298766396660f75
kite <943443868@qq.com>
2020-04-02 11:00:46 +0800

    错误修复

--- vue.config.js
+++ vue.config.js
@@ -1,47 +1,34 @@
 const path = require('path')
-function resolve (dir) {
-  return path.join(__dirname, dir)
+
+function resolve(dir) {
+	return path.join(__dirname, dir)
 }
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/static/mobile'
-    : '/',
-  outputDir: 'dist',
-  assetsDir: 'static',
-  indexPath: 'index.html',
-  filenameHashing: false,
-  devServer: {
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8082',
-        pathRewrite: {
+	publicPath: process.env.NODE_ENV === 'production' ?
+		'/static/mobile' : '/',
+	outputDir: 'dist',
+	assetsDir: 'static',
+	indexPath: 'index.html',
+	filenameHashing: false,
+	devServer: {
+		open: true,
+		proxy: {
+			'/api': {
+				target: 'http://127.0.0.1:8082',
+				pathRewrite: {
 					'^/api': '/'
 				},
-        changeOrigin: true,
-      },
-    }
-  },
-  chainWebpack: config => {
-    // alias
-    config.resolve.alias
-      .set('@', resolve('src'))
-			.set('imgs', resolve('src/assets/images'))
-    // global stylus settings
-    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
-    types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
-  },
-}
-
-
-// 全局stylus配置引入
-function addStyleResource (rule) {  
-  rule.use('style-resource')
-    .loader('style-resources-loader')
-    .options({
-      patterns: [
-        path.resolve(__dirname, './src/assets/styles/globalSettings.styl'),
-      ],
-    })
+				changeOrigin: true,
+			},
+		}
+	},
+	configureWebpack: {
+		resolve: {
+			alias: {
+				'@': path.join(__dirname, 'src'),
+				'imgs': path.join(__dirname, 'src/assets/images'),
+			}
+		}
+	},
 }

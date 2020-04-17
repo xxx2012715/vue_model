20270a47458846501c2190813481f0f8369bace2
kite <943443868@qq.com>
2020-04-17 09:14:57 +0800

    全局样式配置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,4 +17,15 @@ export default {
 }
 </script>
 
+<style lang="stylus" scoped>
+.home
+	color $bgColor
+	position absolute
+	top 0
+	right 0
+	bottom 0
+	left 0
+	// background url('~imgs/bg.png') 100% 100% no-repeat
+
+</style>
 
--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
 	indexPath: 'index.html',
 	filenameHashing: false,
 	devServer: {
-		open: true,
+		open: false,
 		proxy: {
 			'/api': {
 				target: 'http://127.0.0.1:8082',
@@ -23,12 +23,25 @@ module.exports = {
 			},
 		}
 	},
-	configureWebpack: {
-		resolve: {
-			alias: {
-				'@': path.join(__dirname, 'src'),
-				'imgs': path.join(__dirname, 'src/assets/images'),
-			}
-		}
+	chainWebpack: config => {
+	  // alias
+	  config.resolve.alias
+	    .set('@', resolve('src'))
+	    .set('imgs', resolve('src/assets/images'))
+	  // global stylus settings
+	  const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+	  types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
 	},
+}
+
+
+// 全局stylus配置引入
+function addStyleResource (rule) {  
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/styles/globalSettings.styl'),
+      ],
+    })
 }
